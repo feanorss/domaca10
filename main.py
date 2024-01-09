fc420e6cbc0fa6bd3d3b6ed27e6d3ff5ffd9167a
feanorss <madro.milos@gmail.com>
2024-01-09 21:19:52 +0100

    domaca zaciatok

---
+++ main.py
@@ -0,0 +1,8 @@
+class City:
+    def __init__(self, cityname, regionname, countryname, numberofcitiziens, zipcode, areacode):
+        self.cityname = cityname
+        self.regionname = regionname
+        self.countryname = countryname
+        self.numberofcitiziens = numberofcitiziens
+        self.zipcode = zipcode
+        self.areacode = areacode
