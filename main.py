b4cc454b39cfcc88032253268107fb0b00470862
feanorss <madro.milos@gmail.com>
2024-01-16 15:22:50 +0100

    zmena doma

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ class City:
         self.numberofcitiziens = numberofcitiziens
         self.zipcode = zipcode
         self.areacode = areacode
+
+    def getfulladress(self):
+        print(f"Názov mesta je: {self.cityname}, nachadza sa v  kraji: {self.regionname}, v state: {self.countryname}, ma {self.numberofcitiziens} obyvatelov. Postove smerove cilso je {self.zipcode} a area code je {self.areacode}")
+
+bratislava = City("Bratislava","Bratislavsky","Slovakia",500000,82105,50)
+bratislava.getfulladress()
+Humenne = City("Humenne","Presovsky","Slovakia",35000,"06601,5)
+Humenne.getfulladress()
